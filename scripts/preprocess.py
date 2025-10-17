71aef9ca640d7899586d42f393842d1b7639ed1b
Nicolas Danquigny <nicolas.danquigny@outlook.fr>
2025-10-17 15:02:15 +0700

    initialisation des données

---
+++ scripts/preprocess.py
@@ -0,0 +1,33 @@
+from dotenv import load_dotenv
+import os
+from pathlib import Path
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+load_dotenv()
+
+DATA_PATH = Path(os.getenv("DATA_PATH"))
+PROCESSED_DIR = Path(os.getenv("PROCESSED_DIR"))
+SEED = int(os.getenv("SEED"))
+
+df = pd.read_csv(DATA_PATH)
+
+Path(PROCESSED_DIR).mkdir(parents=True, exist_ok=True)
+
+# print(f"PREPROCESS : ✅ Data loaded: {df.shape[0]} rows × {df.shape[1]} columns")
+# print(df.head())
+
+# print(f"PREPROCESS : types: {df.dtypes}")
+# print(f"PREPROCESS : missing values: {df.isna().sum()}")
+
+# print(f"PREPROCESS : success counts: {df['success'].value_counts(normalize=True)}")
+
+x=df.drop(columns=["success"])
+y=df["success"]
+
+x_train, x_test, y_train, y_test = train_test_split(x,y, test_size=0.2, stratify=y, random_state=SEED)
+
+x_train.to_csv(PROCESSED_DIR/"x_train.csv", index= False)
+x_test.to_csv(PROCESSED_DIR/"x_test.csv", index= False)
+y_train.to_csv(PROCESSED_DIR/"y_train.csv", index= False)
+y_test.to_csv(PROCESSED_DIR/"y_test.csv", index= False)
